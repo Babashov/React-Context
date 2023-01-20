6bee2e756131d70db8f066203c4abab3750f8124
Babashov Mahammad <babashov.info@gmail.com>
2023-01-20 20:53:18 +0400

    theme valu stored in localStorage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,10 +1,16 @@
 import { createContext,useState } from "react";
+import { useEffect } from "react";
 
 const ThemeContext = createContext();
 
 
 export const ThemeProvider = ({children})=>{
-    const [theme,setTheme] = useState('dark');
+    const [theme,setTheme] = useState(localStorage.getItem('theme') || "light");
+
+    useEffect(()=>{
+        localStorage.setItem('theme',theme)
+
+    },[theme])
 
     const values = {
         theme,
