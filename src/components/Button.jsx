f94f8eb64a111eb07c23cd3a5cfdb58e8a69bffd
Babashov Mahammad <babashov.info@gmail.com>
2023-01-20 10:05:54 +0400

    added console.log(data) showing data in console

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,6 +4,7 @@ import ThemeContext from "../context/ThemeContext"
 function Button() {
 
 const data = useContext(ThemeContext)
+console.log(data);
 
   return (
     <div>Button theme is: {data}</div>
