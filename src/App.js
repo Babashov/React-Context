ed855fc82e258b85f37ef6cb950e9b34ebe2b250
Babashov Mahammad <babashov.info@gmail.com>
2023-01-20 10:22:23 +0400

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Button from './components/Button';
+import Header from './components/Header';
 import {ThemeProvider} from './context/ThemeContext'
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider>
+        <Header/>
+        <hr/>
         <Button/>
       </ThemeProvider>
     </div>
